a0fd4cecea197ce4932fb4ed0e2f2391a1c71148
CaioCesar123 <c.cesar.ferreira@uol.com.br>
2020-03-03 22:13:14 -0300

    Controller e Pagina criado

---
+++ src/main/java/br/com/senac/controller/AlunoController.java
@@ -0,0 +1,48 @@
+package br.com.senac.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.senac.domain.Aluno;
+import br.com.senac.service.AlunoService;
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Controller
+@RequestMapping("aluno")
+public class AlunoController {
+	
+	@Autowired
+	private AlunoService alunoService;
+	
+	@GetMapping("/ListarAlunos")
+	public ModelAndView listarTodosAlunos() {
+		ModelAndView mv = new ModelAndView("aluno/paginaListarAlunos");
+		mv.addObject("alunos", alunoService.buscarTodosAlunos());
+		return mv;
+	}
+	
+	@GetMapping("/cadastrar")
+	public ModelAndView cadastrarAluno() {
+		ModelAndView mv = new ModelAndView("aluno/cadastrarAluno");
+		mv.addObject("aluno", new Aluno());
+		return mv;
+	}
+	
+	@PostMapping("/salvar")
+	public ModelAndView salvarAluno(Aluno aluno) {
+		alunoService.salvar(aluno);
+		return listarTodosAlunos();
+	}
+	
+	@GetMapping("/alterar/{id}")
+	public ModelAndView alterarAluno(@PathVariable("id") Integer idAluno) throws ObjectNotFoundException{
+		ModelAndView mv = new ModelAndView("aluno/alteraAluno");
+		mv.addObject("aluno", alunoService.buscarPorID(idAluno));
+		return mv;
+	}
+}
