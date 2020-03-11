b109f772f78a1901ffadb8c4b2396a6d397ba4a6
CaioCesar123 <c.cesar.ferreira@uol.com.br>
2020-03-10 21:34:23 -0300

    Professor criado

--- src/main/java/br/com/senac/service/ProfessorService.java
+++ src/main/java/br/com/senac/service/ProfessorService.java
@@ -1,12 +1,14 @@
 package br.com.senac.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.senac.domain.Professor;
 import br.com.senac.repository.ProfessorRepositorio;
+import javassist.tools.rmi.ObjectNotFoundException;
 
 @Service
 public class ProfessorService {
@@ -19,6 +21,22 @@ public class ProfessorService {
 	}
 	public Professor salvar(Professor professor) {
 		return repoProfessor.save(professor);
+	}
+	
+	public Professor buscaID(Integer id) throws ObjectNotFoundException{
+		Optional<Professor> prof = repoProfessor.findById(id);
+		return prof.orElseThrow(() -> new ObjectNotFoundException("Professor Não Encontrado"));
+	}
+	
+	public Professor Alterado(Professor professorNovo) throws ObjectNotFoundException{
+		Professor professor = buscaID(professorNovo.getId());
+		professor.setId(professorNovo.getId());
+		professor.setNome(professorNovo.getNome());
+		return salvar(professor);
+	}
+	
+	public void excluir(Integer id) {
+		repoProfessor.deleteById(id);
 	}
 
 }
